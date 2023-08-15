9d743a216295b73afd58a9147c706bd4626e2ca6
Riley Murray <rileymurray1324@gmail.com>
2023-08-15 11:33:22 -0600

    Update traindata_exploration_quantiles.py
    Added the rest of the plotting functions to the script.

--- amplitude_analysis/traindata_exploration_quantiles.py
+++ amplitude_analysis/traindata_exploration_quantiles.py
@@ -15,11 +15,25 @@ import matplotlib.pyplot as plt
 
 from patsy import dmatrices
 
+from statsmodels.stats.outliers_influence import summary_table
+from statsmodels.sandbox.regression.predstd import wls_prediction_std
+
 #read in our training data
 train_file = "~/repos/seismology_data/woodanderson_train.csv"
 data = pd.read_csv(train_file)
 
 y_train, x_train = dmatrices('diff_pick_ot_time ~ source_receiver_distance', data, return_type = 'dataframe')
+x = data['source_receiver_distance']
+y = data['diff_pick_ot_time']
+
+x1 = pd.DataFrame({'source_receiver_distance':np.linspace(data.source_receiver_distance.min(), data.source_receiver_distance.max())})
+
+poly_1st_order = smf.ols('diff_pick_ot_time ~ source_receiver_distance', data).fit()
+poly_2nd_order = smf.ols('diff_pick_ot_time ~ source_receiver_distance + I(source_receiver_distance ** 2)', data).fit()
+poly_3rd_order = smf.ols('diff_pick_ot_time ~ source_receiver_distance + I(source_receiver_distance ** 3)', data).fit()
+
+prstd, iv_l, iv_u = wls_prediction_std(poly_2nd_order)
+st, dat, ss2 = summary_table(poly_2nd_order, alpha = 0.05)
 
 def conditional_means_scatter():
     """ 
@@ -105,12 +119,132 @@ def multi_quantile_fitlines():
 
     plt.legend(handles=handles)
     plt.show()
+    
+def poly_fitting(): 
+    """ 
+    Plots a scatterplot of the source receiver distance vs the diff pick ot time.
+    The plot includes the polynomial fit lines of first, second, and third order,
+    as well as the resultant models' R-squared values, calculated using OLS regression.
+    """
+    
+    plt.figure(figsize = (8,6))
+    plt.plot(x, y, 'o', alpha = 0.2)
+    plt.title("Signal Velocity - Polynomial Fit")
+    plt.plot(x1.source_receiver_distance,
+             poly_1st_order.predict(x1),
+             'r-',
+             label = '1st order poly fit, $R^2$=%.2f' % poly_1st_order.rsquared)
+    plt.plot(x1.source_receiver_distance,
+             poly_2nd_order.predict(x1),
+             'b-',
+             label = '2nd order poly fit, $R^2$=%.2f' % poly_2nd_order.rsquared)
+    plt.plot(x1.source_receiver_distance,
+             poly_3rd_order.predict(x1),
+             'g-',
+             label = '3rd order poly fit, $R^2$=%.2f' % poly_3rd_order.rsquared)
+    
+    plt.legend(loc = "upper center", fontsize = 14)
+    plt.show()
+    
+def second_order_confidence():
+    """ 
+    Plots a scatterplot of the source receiver distance vs the diff pick ot time.
+    The plot includes second-order polynomial fit line calculated using OLS regression,
+    as well as the 95% prediction and confidence intervals..
+    """
+    
+    plt.figure(figsize=(8,6))
+    plt.plot(x, y, 'o', alpha = 0.2, label = "")
+    plt.title("Signal Velocity - Confidence Intervals")
+    plt.plot(x1.source_receiver_distance, poly_2nd_order.predict(x1), 'k-', label = "2nd order polynomial fit")
 
+    fittedvalues = dat[:,2]
+    predict_mse = dat[:,3]
+    predict_mean_ci_low, predict_mean_ci_upp = dat[:,4:6].T
+    predict_ci_low, predict_ci_upp = dat[:,6:8].T
+
+    data_intervals = {'source_receiver_distance': x,
+                      'srd_low': predict_ci_low,
+                      'srd_upp': predict_ci_upp,
+                      'conf_low': predict_mean_ci_low,
+                      'conf_high': predict_mean_ci_upp}
+    df_intervals = pd.DataFrame(data = data_intervals)
+    df_intervals_sort = df_intervals.sort_values(by = "source_receiver_distance")
+    
+    plt.plot(df_intervals_sort.source_receiver_distance,
+             df_intervals_sort.srd_low,
+             color='r', linestyle='dashed',
+             linewidth=2, label='95% prediction interval')
+    
+    plt.plot(df_intervals_sort.source_receiver_distance,
+             df_intervals_sort.srd_upp,
+             color='r', linestyle='dashed',
+             linewidth=2, label='')
+
+    plt.plot(df_intervals_sort.source_receiver_distance,
+             df_intervals_sort.conf_low,
+             color='c', linestyle='dashed',
+             linewidth=2, label='95% confidence interval')
+
+    plt.plot(df_intervals_sort.source_receiver_distance,
+             df_intervals_sort.conf_high,
+             color='c', linestyle='dashed',
+             linewidth=2, label='')
+
+    plt.legend(fontsize = 14)
+    plt.show()    
+
+
+def polynomial_quantile_regression():
+    #Now that we've visualized the confidence intervals around the 2nd-order polynomial fit, 
+    #we can extend this quantile regression.
+    mod = smf.quantreg(formula='diff_pick_ot_time ~ source_receiver_distance + I(source_receiver_distance ** 2)', data=data)
+    
+    # Quantile regression for 5 quantiles
+    quantiles = [.05, .25, .50, .75, .95]
+    
+    # get all results in a list
+    res_all = [mod.fit(q=q) for q in quantiles]
+    
+    #fit the model
+    res_ols = smf.ols('diff_pick_ot_time ~ source_receiver_distance + I(source_receiver_distance ** 2)', data).fit()
+    
+    plt.figure(figsize=(8,6))
+    
+    # create x for prediction
+    x_p = np.linspace(data['source_receiver_distance'].min(), data['source_receiver_distance'].max(), 50)
+    df_p = pd.DataFrame({'source_receiver_distance': x_p})
+    
+    print("Generating curves...")
+    for qm, res in zip(quantiles, res_all):
+        # get prediction for the model and plot
+        # here we use a dict which works the same way as the df in ols
+        plt.plot(x_p, res.predict({'source_receiver_distance': x_p}), linestyle='--', lw=1, color='k')
+
+        #apply the model
+        y_ols_source_receiver_distance = res_ols.predict(df_p['source_receiver_distance'])
+        #add model results to figure
+        plt.plot(x_p, y_ols_source_receiver_distance, color='red', label='OLS')
+        plt.scatter(data['source_receiver_distance'], data['diff_pick_ot_time'], alpha=.2)
+        plt.xlabel("Distance (km)")
+        plt.ylabel("Time (s)")
+        plt.xlim((0, 250))
+        plt.ylim((-10, 80))
+        plt.title('QUANTILE REGRESSION', fontsize=14)
+        
+    plt.show()
+        
 if __name__ == "__main__":
     #run conditional_means_scatter
     conditional_means_scatter()
     #run multi_quantile_fitlines
     multi_quantile_fitlines()
+    #run poly_fitting
+    poly_fitting()
+    #run second_order_confidence
+    second_order_confidence()
+    #run polynomial_quantile_regression
+    polynomial_quantile_regression()
     
     
     
